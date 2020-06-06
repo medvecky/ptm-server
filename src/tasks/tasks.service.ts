b90990d176e196339d68114bc23a81c72fc8c08f
medvecky <alexey.medvecky@gmail.com>
2020-06-07 00:03:36 +0300

    Adding createTask and getAllTasks endpoints with array storage

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@nestjs/common';
+import {Task} from "./task.model";
+import {TaskStatus} from "./task.status.enum";
+import { v4 as uuid} from 'uuid';
+import {CreateTaskDto} from "./create-task.dto";
+
+@Injectable()
+export class TasksService {
+    private tasks: Task[] = [];
+
+    getAllTasks(): Task[] {
+        return this.tasks;
+    }
+
+    createTask(createTaskDto: CreateTaskDto): Task {
+        const {title, description} = createTaskDto;
+        const task: Task = {
+            id: uuid(),
+            title,
+            description,
+            status: TaskStatus.OPEN,
+        };
+        this.tasks.push(task);
+        return task;
+    }
+}

9f5ac4bb4e4837c91a71f5c84ed1b4f750b14666
medvecky <alexey.medvecky@gmail.com>
2020-07-09 22:03:31 +0300

    Adding request / response logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,25 @@
-import { Module } from '@nestjs/common';
-import { TasksModule } from './tasks/tasks.module';
+import {Module} from '@nestjs/common';
+import {TasksModule} from './tasks/tasks.module';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {typeOrmConfig} from "./config/typeorm.config";
-import { AuthModule } from './auth/auth.module';
-import { ProjectsModule } from './projects/projects.module';
+import {AuthModule} from './auth/auth.module';
+import {ProjectsModule} from './projects/projects.module';
+import {LoggingInterceptor} from '@algoan/nestjs-logging-interceptor';
+import {APP_INTERCEPTOR} from "@nestjs/core";
 
 @Module({
-  imports: [
-      TasksModule,
-      TypeOrmModule.forRoot(typeOrmConfig),
-      AuthModule,
-      ProjectsModule
-  ],
+    providers: [
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: LoggingInterceptor,
+        },
+    ],
+    imports: [
+        TasksModule,
+        TypeOrmModule.forRoot(typeOrmConfig),
+        AuthModule,
+        ProjectsModule
+    ],
 })
-export class AppModule {}
+export class AppModule {
+}
